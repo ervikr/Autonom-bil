592630e2b4fa1c88089ddb98541ff29534181639
ervikr <danerriiber@gmail.com>
2024-03-18 13:44:41 +0100

    Lines

--- camera_test.py
+++ camera_test.py
@@ -1,5 +1,11 @@
 import cv2
 import numpy as np
+import math
+
+theta=0
+minLineLength = 5
+maxLineGap = 10
+maxSlider = 10
 
 cap = cv2.VideoCapture(0)
 
@@ -31,8 +37,24 @@ while(True):
 
     result = cv2.bitwise_and(result, result, mask=full_mask)
 
+    canny = cv2.Canny(result, 100, 200)
+
+    lines = cv2.HoughLinesP(canny, 1, np.pi/180, maxSlider, minLineLength, maxLineGap)
+
+    try:
+        for x in range(0, len(lines)):
+            for x1,y1,x2,y2 in lines[x]:
+                # draw line in image using cv2.line function.
+                cv2.line(image,(x1,y1),(x2,y2),(255,0,0),3)
+                theta=theta+math.atan2((y2-y1),(x2-x1))
+                print(theta)
+    except:
+        print("No lines detected")
+
+
     cv2.imshow('mask', full_mask)
     cv2.imshow('result', result)
+    cv2.imshow('canny', canny)
 
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
